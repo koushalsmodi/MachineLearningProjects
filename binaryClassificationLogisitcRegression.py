5779d753a3e687917d927f08770bb07b22a528e9
Koushal S Modi <124229657+koushalsmodi@users.noreply.github.com>
2024-03-03 19:47:49 -0800

    Update binaryClassificationLogisitcRegression.py

--- binaryClassificationLogisitcRegression.py
+++ binaryClassificationLogisitcRegression.py
@@ -1,4 +1,4 @@
-
+# importing libraries
 import numpy as np
 import sklearn as sk
 import sklearn.datasets
@@ -6,16 +6,21 @@ import sklearn.linear_model
 import sklearn.model_selection
 from pprint import pprint
 
+# loading the breast cancer dataset
 dataset = sk.datasets.load_breast_cancer()
 
 X = dataset.data
 y = dataset.target
 
+# splitting the dataset into training and validation sets
 X_train, X_val, y_train, y_val = sk.model_selection.train_test_split(X, y)
 
+# training the model using scikit-learn
 model = sk.linear_model.LogisticRegression()
 model.fit(X_train, y_train)
 y_pred = model.predict(X_val)
+
+# measuring accuracy, that is, how well do the predicted probabilities agree with the ground truth probability.
 accuracy = np.mean(y_pred == y_val)
 print(f'accuracy is: {accuracy}') # 0.9370629370629371
 
