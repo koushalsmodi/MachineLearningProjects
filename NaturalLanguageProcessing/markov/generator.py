19c8c3d5aa446d97a99155feacdcdd8c7e8974ee
koushalsmodi <koushalsmodi@gmail.com>
2025-06-30 18:28:30 -0700

    markov models for text

---
+++ NaturalLanguageProcessing/markov/generator.py
@@ -0,0 +1,23 @@
+# builds Markov models from text and can generate new, similar-sounding text
+import markovify
+import sys
+
+# Read the text from the file
+if len(sys.argv) != 2:
+  sys.exit("Usage: python3 generator.py sample.txt")
+
+with open(sys.argv[1]) as f:
+  text = f.read()
+
+# Train model
+# Create a markov chain model from text input
+# model learns which words are likely to follow others based on the structure of the sample.txt
+text_model = markovify.Text(text)
+
+# Generate sentences
+print()
+
+# tries to build a grammatically plausible sentence using the probabilities from the model
+for i in range(5):
+  print(text_model.make_sentence())
+  print()
