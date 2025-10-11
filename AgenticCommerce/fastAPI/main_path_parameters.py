a33693c981896de99666b285303fdf675f311fc1
koushalsmodi <koushalsmodi@gmail.com>
2025-10-10 21:18:01 -0700

    working with path parameters in get decorator

---
+++ AgenticCommerce/fastAPI/main_path_parameters.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+from fastapi import FastAPI
+
+class FilePaths(str, Enum):
+    john = "john"
+    koushal = "koushal"
+
+app = FastAPI()
+
+@app.get("/files/{file_name}")
+async def read_file(file_name: FilePaths):
+    if file_name is FilePaths.john:
+        return {"filepath": "/home/johndoe/myfile.txt", "message": "John, this is for you!"}
+    if file_name.value == FilePaths.koushal.value:
+        return {"filepath": "/home/koushalsmodi/ganpati.txt", "message": "Koushal, this one is for you!"}
+
