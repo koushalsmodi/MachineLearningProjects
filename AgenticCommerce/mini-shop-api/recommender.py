48b83bcad5578d18b8708cefeb182b8ac9432a40
koushalsmodi <koushalsmodi@gmail.com>
2025-11-04 18:09:39 -0700

    playing with gemini - correct way

--- AgenticCommerce/mini-shop-api/recommender.py
+++ AgenticCommerce/mini-shop-api/recommender.py
@@ -1,10 +1,13 @@
-import google.genai as genai
-
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
+import google.generativeai as genai
 from dotenv import load_dotenv
 import os 
 
+
+
 load_dotenv()
- 
+genai.configure(api_key = os.getenv("GEMINI_API_KEY"))
 catalog = [
     {"name": "MacBook", "price": 999},
     {"name": "Dell", "price": 650},
@@ -12,9 +15,6 @@ catalog = [
 ]
 user_query = "Recommend a laptop under $700"
 
-api_key = os.getenv("GEMINI_API_KEY")
-
-client = genai.Client(api_key=api_key)
 
 prompt = f"""You are a product recommender and your role is to 
 take user's query: {user_query} and advise the user with a small list or with just 1 item 
@@ -22,15 +22,10 @@ based on the user's budget and preferences
 from the catalog: {catalog} based on the user query.
 The user will be the buyer so it is of utmost importance to provide a correct response
 so as to have our sale successful.
-
-
 Output should be short and human-readable.
 """
 
+model = genai.GenerativeModel("gemini-2.5-pro")
 
-response = client.models.generate_content(
-    model="gemini-2.5-pro", 
-    contents=prompt
-)
-
+response = model.generate_content(prompt)
 print(response.text)
