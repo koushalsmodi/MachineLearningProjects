c7013d7855b02b5bd7b6b414ec8bd8b392faa433
koushalsmodi <koushalsmodi@gmail.com>
2026-05-11 19:10:05 -0700

    python practice

---
+++ NLP/Python/l0_functions_variables/hello.py
@@ -0,0 +1,9 @@
+# Ask user for their name
+name = input("What's your name? ")
+
+# Say hello to user
+print(f"hello, {name}")
+print()
+print("hello,", name)
+print()
+print("hello,", name, sep = " ")
