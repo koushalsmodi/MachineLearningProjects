496207c773d700ff39fae21a6d2e0fc86eab91f8
koushalsmodi <koushalsmodi@gmail.com>
2026-05-13 19:39:11 -0700

    if elif else

---
+++ NLP/Python/l1_conditionals/house.py
@@ -0,0 +1,11 @@
+name = input("What's your name? ")
+
+if name in ["Harry", "Hermoine", "Ron"]: 
+    print("Gryyindor")
+    
+elif name == "Draco":
+    print("Slytherin")
+    
+else:
+    print("Who?")    
+    
